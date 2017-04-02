075ef69174759300ece449d327ddf6ad4c4cb7df
Jason Juhyun Kim <mtinet@hanmail.net>
2017-04-03 02:48:17 +0900

    Add files via upload

---
+++ CdS_led/CdS_led.ino
@@ -0,0 +1,27 @@
+const int ledpin=13;
+const int sensorpin=A0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledpin, OUTPUT);
+}
+
+void loop()
+{
+  int rate = analogRead(sensorpin);
+  Serial.println(rate);
+  
+  if(rate>300)
+  {
+    digitalWrite(ledpin, HIGH);
+  }
+  else
+  {
+
+    digitalWrite(ledpin, HIGH);
+    delay(50);               
+    digitalWrite(ledpin, LOW);    
+    delay(50);
+  }
+}
