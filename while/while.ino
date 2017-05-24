502642460410bee849f3235617378d1e6744a26e
Jason Juhyun Kim <mtinet@hanmail.net>
2017-05-24 10:35:45 +0900

    Add files via upload

---
+++ while/while.ino
@@ -0,0 +1,20 @@
+int led=11;
+int i=0;
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  while(i<255){
+    i=i+1;
+    analogWrite(led, i);
+    delay(20);              
+  }
+  while(i>0){
+    i=i-1;
+    analogWrite(led, i);
+    delay(20);              
+  }
+}
+
