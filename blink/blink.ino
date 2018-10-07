0863eb8e067b70461de5baea535501d94acdfff6
Jason Juhyun Kim <mtinet@hanmail.net>
2018-10-08 01:54:46 +0900

    Update blink.ino

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,12 +1,10 @@
-int led = 13;
-
 void setup() {
-  pinMode(led, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(led, HIGH);   
+  digitalWrite(13, HIGH);   
   delay(1000);             
-  digitalWrite(led, LOW);    
+  digitalWrite(13, LOW);    
   delay(1000);             
 }
